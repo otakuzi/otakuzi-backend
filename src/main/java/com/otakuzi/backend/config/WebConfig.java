320fb840a188cf5f1d60de2e74b98c53036c29b5
Hyormone <ddf998@gmail.com>
2025-12-05 13:02:32 +0900

    feat: CORS 설정 추가

--- src/main/java/com/otakuzi/backend/config/WebConfig.java
+++ src/main/java/com/otakuzi/backend/config/WebConfig.java
@@ -10,7 +10,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/shops/**")
-                .allowedOrigins("http://localhost:3000") // Next.js 기본 포트
+                .allowedOrigins(
+                    "http://otakuim.com",
+                    "http://www.otakuim.com",
+                    "http://51.20.93.193:3000",
+                    "http://localhost:3000"
+                ) // Next.js 기본 포트
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
