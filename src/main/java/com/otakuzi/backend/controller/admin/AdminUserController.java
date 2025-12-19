3e5c966a5a6af70298e265a77057591f5fd20deb
Hyormone <ddf998@gmail.com>
2025-12-19 13:25:49 +0900

    feat: 사용자 조회 api 추가

--- src/main/java/com/otakuzi/backend/controller/admin/AdminUserController.java
+++ src/main/java/com/otakuzi/backend/controller/admin/AdminUserController.java
@@ -1,13 +1,17 @@
 package com.otakuzi.backend.controller.admin;
 
+import com.otakuzi.backend.constant.UserType;
 import com.otakuzi.backend.dto.admin.AdminUserUpdateDto;
 import com.otakuzi.backend.entity.User;
 import com.otakuzi.backend.service.admin.AdminUserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "관리자(Admin)", description = "관리자 페이지입니다.")
 @RestController
@@ -16,6 +20,24 @@ import org.springframework.web.bind.annotation.*;
 public class AdminUserController {
 
     private final AdminUserService adminUserService;
+
+    @GetMapping
+    @Operation(summary = "사용자 수정", description = "사용자를 전체 조회합니다.")
+    public ResponseEntity<List<User>> getAllUsers(
+        @Parameter(description = "검색할 사용자 닉네임(부분 일치)")
+        @RequestParam(required = false) String nickname,
+        @Parameter(description = "검색할 권한 사용자")
+        @RequestParam(required = false) UserType userType
+    ) {
+        if (nickname != null) {
+            return ResponseEntity.ok(adminUserService.adminGetUsersByNickname(nickname));
+        }
+        if (userType != null) {
+            return ResponseEntity.ok(adminUserService.adminGetUsersByUserType(userType));
+        }
+
+        return ResponseEntity.ok(adminUserService.adminGetAllUsers());
+    }
 
     @PutMapping({"/{id}"})
     @Operation(summary = "사용자 수정", description = "사용자 정보를 수정합니다.")
