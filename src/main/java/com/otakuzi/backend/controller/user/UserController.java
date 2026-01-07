1749228ec5caf6d92bfb45f603a6e651d5f338aa
Hyormone <ddf998@gmail.com>
2026-01-07 16:50:09 +0900

    fix: 닉네임 수정 해당 유저만 가능하도록 수정

--- src/main/java/com/otakuzi/backend/controller/user/UserController.java
+++ src/main/java/com/otakuzi/backend/controller/user/UserController.java
@@ -1,6 +1,7 @@
 package com.otakuzi.backend.controller.user;
 
 import com.otakuzi.backend.dto.user.UserNicknameUpdateRequest;
+import com.otakuzi.backend.global.config.auth.PrincipalDetails;
 import com.otakuzi.backend.service.user.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -8,6 +9,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "사용자(User)", description = "사용자 페이지입니다.")
@@ -17,13 +20,19 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
-    @PatchMapping("/{id}/nickname")
-    @Operation(summary = "닉네임 변경", description = "닉네임을 변경합니다. 금지어/중복값 확인")
+    @PatchMapping("/nickname")
+    @Operation(summary = "닉네임 변경", description = "로그인한 사용자의 닉네임을 변경합니다. 금지어/중복값 확인")
     public ResponseEntity<String> updateNickname(
-            @PathVariable Long id,
+            @AuthenticationPrincipal PrincipalDetails principalDetails,
             @RequestBody @Valid UserNicknameUpdateRequest dto) {
+
+        if (principalDetails == null) {
+            throw new IllegalArgumentException("로그인이 필요합니다.");
+        }
+
+        Long id = principalDetails.getUser().getId();
 
         log.info("닉네임 변경 요청 UserID: {}, NewNickname: {}", id, dto.getNickname());
 
