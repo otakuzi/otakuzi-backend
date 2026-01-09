bea3b5879a9ab0c552c64a8f0a7fee2db4ec56e8
Hyormone <ddf998@gmail.com>
2026-01-09 15:29:43 +0900

    fix: 테스트 환경 구축

--- src/test/java/com/otakuzi/backend/OtakuziBackendApplicationTests.java
+++ src/test/java/com/otakuzi/backend/OtakuziBackendApplicationTests.java
@@ -9,7 +9,7 @@ class otakuziBackendApplicationTests {
 	@Test
 	void 고의로_실패하는_테스트() {
 		if (true) {
-			throw new RuntimeException("이 빌드는 실패해야 합니다!");
+			throw new RuntimeException("이 빌드는 실패해야 합니다!ㅇㅇ");
 		}
 	}
 
